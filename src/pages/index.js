06b15878539a02298aaf86c8934fbcd8120594e5
quangtudev <61562160+quangtudev@users.noreply.github.com>
2023-07-20 16:37:14 +0700

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import { Helmet } from 'react-helmet';
+
+import Layout from 'components/Layout';
+import Section from 'components/Section';
+import Container from 'components/Container';
+
+import styles from 'styles/pages/404.module.scss';
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <Helmet>
+        <title>Create Next App</title>
+        <meta name="robots" content="noindex, nofollow" />
+      </Helmet>
+      <Section>
+        <Container className={styles.center}>
+          <h1>Welcome to my website</h1>
+        </Container>
+      </Section>
+    </Layout>
+  );
+}
+
+// Next.js method to ensure a static page gets rendered
+export async function getStaticProps() {
+  return {
+    props: {},
+  };
+}
