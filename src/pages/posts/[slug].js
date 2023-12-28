80c88cb00dad9d051812e89de4a1f4fa47b98963
quangtudev <61562160+quangtudev@users.noreply.github.com>
2023-12-28 14:39:15 +0700

    Update [slug].js

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -124,21 +124,26 @@ export default function Post({ post }) {
 }
 
 export async function getServerSideProps(context) {
-  // Always perform a redirect
-  
-  cons
-const targetURL = `${process.env.WORDPRESS_REDIRECT_DOMAIN}/${encodeURIComponent(context.params.slug)}/`;
-  context.
-  cont
+  let props = {};
+  const referral = context?.req?.headers?.referer;
+  // if (referral?.includes('facebook.com')) {
+  if (referral != null && referral.trim() != '') {
+    // On the server, we'll use an HTTP response to
+    // redirect with the status code of our choice.
+    // 307 is for temporary redirects.
+    const targetURL = `${process.env.WORDPRESS_REDIRECT_DOMAIN}/${encodeURIComponent(context.params.slug)}/`;
+    context.res.writeHead(307, { Location: targetURL });
+    context.res.end();
+  } else {
+    const params = context?.params;
+    const { post } = await getPostBySlug(params?.slug);
 
- 
-res.writeHead(307, { Location: targetURL });
-  context.
-  context
-
-  c
-res.end();
-}
+    if (!post) {
+      return {
+        props: {},
+        notFound: true,
+      };
+    }
 
     const { categories, databaseId: postId } = post;
 
