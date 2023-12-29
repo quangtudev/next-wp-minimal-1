4068643ec7ee57ffba662b4ee0eaa6633bf17669
quangtudev <61562160+quangtudev@users.noreply.github.com>
2023-12-30 01:35:46 +0700

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(req) {
-    const targetURL = `${process.env.WORDPRESS_REDIRECT}/${req.nextUrl.pathname.replace('/posts/', '')}`;
+    const targetURL = `${process.env.WORDPRESS_REDIRECT_DOMAIN}/${req.nextUrl.pathname.replace('/posts/', '')}`;
     return NextResponse.redirect(targetURL);
 }
