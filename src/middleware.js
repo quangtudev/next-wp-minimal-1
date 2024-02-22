05f4942d491c145ea89581ee5926e41adac373db
quangtudev <61562160+quangtudev@users.noreply.github.com>
2024-02-22 13:51:35 +0700

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,11 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(req) {
-    const targetURL = `${process.env.WORDPRESS_REDIRECT_DOMAIN}/${req.nextUrl.pathname.replace('/posts/', '')}`;
-    return NextResponse.redirect(targetURL);
+    let ua = req.headers.get('user-agent');
+    if(ua.includes('facebookexternalhit') || ua.includes('facebookcatalog')){
+        return NextResponse.next();
+    } else {
+        const targetURL = `${process.env.WORDPRESS_REDIRECT_DOMAIN}/${req.nextUrl.pathname.replace('/posts/', '')}`;
+        return NextResponse.redirect(targetURL);
+    }   
 }
